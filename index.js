22edcaa5104177f6883a70f00b1a345346f014c5
i-webtest <stasbass80@gmail.com>
2024-05-01 12:48:53 +0300

    Add: gsap animate

---
+++ index.js
@@ -0,0 +1,54 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const animateFrom = (elem, direction) => {
+  direction = direction || 1;
+  var x = 0,
+    y = direction * 100;
+  if (elem.classList.contains('gs_reveal_fromLeft')) {
+    x = -100;
+    y = 0;
+  } else if (elem.classList.contains('gs_reveal_fromRight')) {
+    x = 100;
+    y = 0;
+  }
+  elem.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  elem.style.opacity = '0';
+  gsap.fromTo(
+    elem,
+    { x: x, y: y, autoAlpha: 0 },
+    {
+      duration: 2,
+      x: 0,
+      y: 0,
+      autoAlpha: 1,
+      ease: 'expo',
+      overwrite: 'auto',
+    },
+  );
+};
+
+const hide = (elem) => {
+  gsap.set(elem, { autoAlpha: 0 });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray('.gs_reveal').forEach((elem) => {
+    hide(elem); // assure that the element is hidden when scrolled into view
+
+    ScrollTrigger.create({
+      trigger: elem,
+      markers: false,
+      onEnter: function () {
+        animateFrom(elem);
+      },
+      onEnterBack: function () {
+        animateFrom(elem, -1);
+      },
+      onLeave: function () {
+        hide(elem);
+      }, // assure that the element is hidden when scrolled into view
+    });
+  });
+});
